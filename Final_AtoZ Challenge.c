019036bef8e13423a8141273c56439e332fdb982
s7070018 <s7070018@kmitl.ac.th>
2015-03-31 16:55:51 +0700

    up

--- Final_AtoZ Challenge.c
+++ Final_AtoZ Challenge.c
@@ -7,15 +7,15 @@
 #include <ctype.h>
 #include <time.h>
 #include <unistd.h>
-#include <dos.h>
-
+#include <math.h>
 
 void ClearConsoleToColors(int ForgC, int BackC);
 void gotoxy(int x, int y);
 void wait( int sec );
 void load();
-void play_gate_time();
+void play_state();
 void character(char num);
+void number_in(char num,int x,int y);
 void right();
 void left();
 void Read_file(char*dir, char*type, int x, int y);
@@ -26,24 +26,49 @@ void Read_file(char*dir, char*type, int x, int y);
 
 int main()
 {
-    //ClearConsoleToColors(17, 15);
-   // load();
-    play_gate_time();
-
-/*
-    system("cls");
-    char a;
-    printf("Please put character : ");
-    scanf("%c",&a);
-    character(a);*/
-
+    ClearConsoleToColors(11, 16);
+    load();
+    play_state();
+    /*Read_file("time spent.txt","r",31,5);
+    Read_file("number.txt","r",17,11);*/
 
     getch();
 
 }
+
+void number_in(char num,int x,int y)
+{
+    char c;
+    char *cha[27] = {'0','1','2','3','4','5','6','7','8','9'};
+    int count = 0;
+    FILE *info;
+    info=fopen("number.txt","r");
+    while(cha[count]!=num){
+        count++;
+    }
+    while(1){
+        c=getc(info);
+        if(c==cha[count])
+        {
+            do{
+                c=getc(info);
+                if(c!=cha[count+1] ){
+                    printf("%c",c);
+                    if(c=='\n'){
+                        y++;
+                        gotoxy(x,y);
+                    }
+                }
+            }while(c!=cha[count+1] && c!=EOF);
+            break;
+        }
+    }
+    fclose(info);
+}
+
 void character(char num)
 {
-    char c,l,r;
+    char c;
     char *cha[27] = {'a','b','c','d','e','f','g','h','i','j','k','l'\
     ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int count = 0,index=7;
@@ -72,38 +97,24 @@ void character(char num)
     fclose(info);
 }
 
-void play_gate_time()
+void play_state()
 {
+    home:
     system("cls");
     int i,j,k,check=50,sec=10,index_cha=0,index_t=7;
     int r1=42,r2=30;
+    float count = 0.0;
+
     j=sec;
     char *cha[27] = {'a','b','c','d','e','f','g','h','i','j','k','l'\
     ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char time_s[5],keep[5];
+    strcpy(keep,"0");
     int key = 0;
-    gotoxy(20,24);  printf("Please put your keyboard : A to Z");
-    gotoxy(20,25);  printf("Please put your keyboard : <spacebar>");
-    //gotoxy(5,3);    printf("Time : ");
-    //gotoxy(12,3);   printf("%.2d",sec);
-    gotoxy(20,3);
 
-    //500000000
+    gotoxy(24,12);  printf("Please press your keyboard : A to Z");
+    gotoxy(17,13);  printf("If you are ready press your keyboard : <spacebar>");
 
-/*
-    for(i=1;i<=10;i++){
-    	for(j=0;j<=500000000;j++);{
-            for(k=0;k<=5;k++)
-                printf("%c",178);
-    	}
-    }*/
-
-
-
-
-   /* for(j=0;j<=check;j++){
-        printf("%c",177);
-    }
-*/
     while(1)
     {
         if (_kbhit())
@@ -116,22 +127,35 @@ void play_gate_time()
         }
     }
 
+    clock_t begin, end;
+    double time_spent;
+
+    begin = clock();
+
     character(cha[index_cha]);
+
      while(1){
+        gotoxy(17,20);  printf("If you want to restart put your keyboard : <spacebar>");
         if (_kbhit())
         {
+            gotoxy(17,20);  printf("If you want to restart put your keyboard : <spacebar>");
             key =_getch();
+            if (key == ' '){
+                goto home;
+                break;}
 
             if (key == cha[index_cha]){
                 character(cha[index_cha]);
-                Beep(400, 10);
+                Beep(400, 50);
 
-                for(i=1;i<=15;i++){
+                for(i=1;i<=7;i++){
                     system("cls");
                     Read_file("right.txt","r",r1+i,9);
                     Read_file("left.txt","r",r2-i,9);
-                    wait(0.9);
+                    gotoxy(17,20);  printf("If you want to restart put your keyboard : <spacebar>");
                 }
+                if(key == 'z')
+                    break;
 
                 index_cha++;
                 system("cls");
@@ -139,87 +163,45 @@ void play_gate_time()
                 }
             else
                 character(cha[index_cha]);
+                gotoxy(17,20);  printf("If you want to restart put your keyboard : <spacebar>");
 
         }
+
      }
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    system("cls");
 
-//--------------------------------------------------------- mix ----------------------------------------
- /*   character(cha[index_cha]);
+//----------------------------End Play ------------------------------
+    printf("Time : %.2f",time_spent);
+    sprintf(time_s,"%.2f",time_spent);
+    if(time_spent < 10)
+        strcat(keep,time_s);
+    else
+        strcpy(keep,time_s);
+    int indextx = 17;
 
-    while(1){
-        system("cls");
-        character(cha[index_cha]);
-        if (_kbhit())
-        {
-            gotoxy(9,3);    printf("Time : ");
-            gotoxy(16,3);   printf("%.2d",sec);
-            key =_getch();
-            system("cls");
-            if (key == cha[index_cha]){
-                character(cha[index_cha]);
-
-                for(i=1;i<=12;i++){
-                    system("cls");
-                    gotoxy(20,3);
-                    for(j=0;j<=check;j++){
-                        printf("%c",177);
-                    }
-                    gotoxy(9,3);    printf("Time : ");
-                    gotoxy(16,3);   printf("%.2d",sec);
-                    Read_file("right.txt","r",r1+i,9);
-                    Read_file("left.txt","r",r2-i,9);
-                    wait(0.9);
-                }
-
-
-                index_cha++;
-                system("cls");
-                character(cha[index_cha]);
-                }
-
-        }
-        else{
-            system("cls");
-            character(cha[index_cha]);
-            if(sec <= 0){
-                system("cls");
-                Read_file("time_out.txt","r",2,9);
-                break;}
-
-            gotoxy(9,3);    printf("Time : ");
-            gotoxy(16,3);   printf("%.2d",sec);
-            gotoxy(20,3);
-            for(j=0;j<=check;j++){
-                printf("%c",177);
-            }
-            sec--;  check-=5;
-            wait(1);
-
-
+    Read_file("time spent.txt","r",31,5);
+    for(i=0;i<2;i++){
+        if(keep[i] >= 48 && keep[i] <= 57){
+            number_in(keep[i],indextx,11);
+            indextx += 10;
         }
     }
-*/
-//--------------------------------------------- Time--------------------------------
-/*
-    while(1){
-        system("cls");
 
-        if(sec <= 0){
-            system("cls");
-            Read_file("time_out.txt","r",2,9);
-            break;}
-
-        gotoxy(9,3);    printf("Time : ");
-        gotoxy(16,3);   printf("%.2d",sec);
-        gotoxy(20,3);
-        for(j=0;j<=check;j++){
-            printf("%c",177);
+    Read_file("symbol.txt","r",indextx,11);
+    indextx+=10;
+    for(i=2;i<5;i++){
+        if(keep[i] >= 48 && keep[i] <= 57){
+            number_in(keep[i],indextx,11);
+            indextx += 10;
         }
-        sec--;  check-=5;
-        wait(1);
-    }*/
-}
+    }
 
+
+
+
+}
 void Read_file(char* dir, char*type, int x, int y)
 {
     char c;
@@ -266,7 +248,7 @@ void wait( int sec )
 {
      clock_t end_wait;
      end_wait = clock() + sec * CLK_TCK;
-     while (clock() < end_wait){usleep(1000);}
+     while (clock() < end_wait){}
 }
 
 
